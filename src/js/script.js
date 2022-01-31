ec7b13a744c3912e9fabd57f98f855dafec57afc
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-01-31 15:54:26 +0300

    Адаптив

--- src/js/script.js
+++ src/js/script.js
@@ -57,16 +57,37 @@ function printText( el ){
 			i++;
 		}
 
-	print__fn() // init
+	print__fn() 
 };
 
-// init
+
 let elements = document.getElementsByClassName( 'my-text' )
 let elemArray = Array.from(elements);
 elemArray.forEach((item) => printText(item));
 
-				
 
+
+// Увеличение картинки				
+$(function(){
+	$('.minimized').click(function(event) {
+	  var i_path = $(this).attr('src');
+	  $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+	  $('#magnify').css({
+		left: ($(document).width() - $('#magnify').outerWidth())/2,
+		// top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
+				 top: ($(window).height() - $('#magnify').outerHeight())/2
+	 });
+	  $('#overlay, #magnify').fadeIn('fast');
+	});
+	
+	$('body').on('click', '#close-popup, #overlay', function(event) {
+	  event.preventDefault();
+ 
+	  $('#overlay, #magnify').fadeOut('fast', function() {
+		 $('#close-popup, #magnify, #overlay').remove();
+	  });
+	});
+ });
 
 
 
